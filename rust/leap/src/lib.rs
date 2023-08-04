f4d89e06b39499497a6cf54e0c350969fc786355
mekhi <lyman.von9506@gmail.com>
2023-08-04 08:45:08 +0800

    refactor: Refactor the leap exercise

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,13 +1,8 @@
 pub fn is_leap_year(year: u64) -> bool {
-    if year % 4 == 0 {
-        if year % 100 == 0 {
-            if year % 400 == 0 {
-                return true;
-            }
-            return false;
-        }
-        return true;
+    match (year % 4, year % 100, year % 400) {
+        (0, 0, 0) => true,
+        (0, 0, _) => false,
+        (0, _, _) => true,
+        _ => false,
     }
-
-    false
 }
